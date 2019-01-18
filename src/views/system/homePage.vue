a6952fe06239d1375cb3f419681fd42077561ba2
zhangkai <1198219721@qq.com>
2019-01-18 12:30:03 +0800

    侧边栏导航3 修复小bug

--- src/views/system/homePage.vue
+++ src/views/system/homePage.vue
@@ -42,18 +42,48 @@ import NavMenu from '../../components/NavMenu'
       return {
         currentMenu:'',
         isCollapse: false,
-        leftMenus:{}
+        leftMenus:{
+            "code":0,                //类型：Number  必有字段  备注：无
+    "msg":"success",                //类型：String  必有字段  备注：无
+    "childs": [                //类型：Array  必有字段  备注：一级菜单数组
+         {                //类型：Object  必有字段  备注：无
+            "id":16,                //类型：Number  必有字段  备注：id
+            "name":"首页",                //类型：String  必有字段  备注：菜单名
+            "pid":0,                //类型：Number  必有字段  备注：父菜单
+            "depth":1,                //类型：Number  必有字段  备注：菜单层级 1：一级，2：二级
+            "path":"/system/homepage/a",                //类型：String  必有字段  备注：vue路径
+            "icon":"el-icon-star-off"                //类型：Mixed  必有字段  备注：图标class
+        },
+         {                //类型：Object  必有字段  备注：一级菜单
+            "id":15,                //类型：Number  必有字段  备注：id
+            "name":"信誉管理",                //类型：String  必有字段  备注：菜单名
+            "pid":0,                //类型：Number  必有字段  备注：父菜单id
+            "depth":1,                //类型：Number  必有字段  备注：菜单层级 1：一级，2：二级
+            "path":"#",                //类型：String  必有字段  备注：vue路径
+            "icon":"el-icon-message",                //类型：Mixed  必有字段  备注：图标class
+            "childs": [                //类型：Array  必有字段  备注：子菜单数组
+                 {                //类型：Object  必有字段  备注：子菜单
+                    "id":17,                //类型：Number  必有字段  备注：子菜单id
+                    "name":"学生信誉",                //类型：String  必有字段  备注：子菜单名称
+                    "pid":15,                //类型：Number  必有字段  备注：父菜单id
+                    "depth":2,                //类型：Number  必有字段  备注：菜单层级 1：一级，2：二级
+                    "path":"/system/homepage/b",                //类型：String  必有字段  备注：vue路径
+                    "icon":"el-icon-loading"                //类型：Mixed  必有字段  备注：图标class
+                }
+            ]
+        }
+    ]
+        }
     }
   },
 
     mounted(){
-        let currentUrl = window.location.href;
-        this.currentMenu = currentUrl.split('/#')[1];  
-        let self = this;
+        // let self = this;
+        this.currentMenu = document.location.pathname;
         this.get(ApiPath.system.getMenu)
         .then(function(res){
-            if(res.data.code == 0)
-                self.leftMenus = res.data;
+            // if(res.data.code == 0)
+            //     self.leftMenus = res.data;
         })
         .catch(function(err){
           console.log(err);
@@ -73,7 +103,7 @@ import NavMenu from '../../components/NavMenu'
     padding: 0;
   }
   .el-submenu .el-menu-item{
-      min-width: 180px;
+      min-width: 180px !important;
   }
 
 .header_css{
