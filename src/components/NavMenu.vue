c70e3778e5aca82b81ac25313157ac78abf35437
zhangkai <1198219721@qq.com>
2019-01-18 11:44:56 +0800

    解决冲突1.18.2

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -17,23 +17,6 @@
         <NavMenu :navMenus="navMenu.childs"></NavMenu>
       </el-submenu>
     </label>
-        <!-- <label v-for="(navMenu,key) in navMenus" :key="key">
-      <el-menu-item v-if="navMenu.childs==null&&navMenu.entity"
-                    :key="navMenu.entity.id" :data="navMenu" :index="navMenu.entity.value" :route="navMenu.entity.value" :default-active="$route.path">
-        <i :class="navMenu.entity.icon"></i>
-        <span slot="title" class="item_title" >{{navMenu.entity.alias}}</span>
-      </el-menu-item>
-
-      <el-submenu v-if="navMenu.childs&&navMenu.entity"
-                  :key="navMenu.entity.id" :data="navMenu" :index="navMenu.entity.name">
-        <template slot="title">
-          <i :class="navMenu.entity.icon"></i>
-          <span class="item_title"> {{navMenu.entity.alias}}</span>
-        </template>
-        <NavMenu :navMenus="navMenu.childs"></NavMenu>
-      </el-submenu>
-    </label> -->
-
   </div>
 </template>
 
